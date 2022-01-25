a59216caf3c91ad21ac3e2b6b23da764a04db257
Ticketing54 <85231269+Ticketing54@users.noreply.github.com>
2022-01-25 13:46:41 +0900

    퀘스트 도중 오브젝트 풀링 작업중

---
+++ portfolio/Assets/script/Manager/PoolingManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager<T>{
+    class PoolingData
+    {
+        public PoolingData(T _prefab)
+        {
+            Prefab = _prefab;
+            Pool = new Queue<T>();
+            Pool.Enqueue(_prefab);
+            count = 1;
+        }
+
+        void Add(T _data)
+        {
+            Pool.Enqueue(_data);
+            
+            
+        }
+        T Prefab;
+        Queue<T> Pool;
+        int count;
+
+        T 
+    }
+
+
+    Dictionary<string, PoolingData> dic = new Dictionary<string, PoolingData>();
+    PoolingData data;
+    public delegate void ResetObj(T _obj);
+    
+    public void Add(string _Key,T _t, ResetObj _Reset)
+    {
+        _Reset(_t);
+
+        if (dic.TryGetValue(_Key,out data))
+        {
+
+        }
+        else
+        {
+            dic.Add(_Key, new PoolingData(_t));            
+        }
+
+        data = null;
+    }
+
+   
+
+
+       
+
+
+}
